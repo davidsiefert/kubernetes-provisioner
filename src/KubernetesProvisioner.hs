15c5769309a57998abd699dfe3704e3f4231a8a9
David Siefert <siefert.david@gmail.com>
2016-02-08 07:14:02 -0600

    Support for extracting kubernetes into home directory.

--- src/KubernetesProvisioner.hs
+++ src/KubernetesProvisioner.hs
@@ -1,23 +1,36 @@
 {-# LANGUAGE OverloadedStrings #-}
 module KubernetesProvisioner where
 
-import Prelude hiding (writeFile)
+import Prelude hiding (writeFile, read, readFile)
 import Turtle (procStrict, Text, empty, ExitCode(..))
 import System.IO.Temp (withSystemTempDirectory)
 import Data.Monoid ((<>))
 import System.FilePath ((</>))
 import Network.Wreq (get, responseBody)
 import Control.Lens ((^.))
-import Data.ByteString.Lazy (writeFile)
+import Data.ByteString.Lazy (readFile, writeFile)
+import System.Directory (getHomeDirectory)
+import Codec.Compression.GZip (decompress)
+import Codec.Archive.Tar (read, unpack)
 
 downloadKubernetes :: IO ()
 downloadKubernetes = do
-  withSystemTempDirectory "kubernetes" downloadReleaseInto
-  where
-    downloadReleaseInto :: FilePath -> IO ()
-    downloadReleaseInto tempPath = do
-      response <- get "https://github.com/kubernetes/kubernetes/releases/download/v1.1.2/kubernetes.tar.gz"
-      writeFile (tempPath </> "kubernetes-1.1.2.tar.gz") (response ^. responseBody)
+  withSystemTempDirectory "kubernetes" (\tempPath -> do
+    kubernetesArchivePath <- downloadReleaseInto tempPath
+    homePath <- getHomeDirectory
+    extractRelease kubernetesArchivePath homePath
+    )
+
+downloadReleaseInto :: FilePath -> IO String
+downloadReleaseInto tempPath = do
+  response <- get "https://github.com/kubernetes/kubernetes/releases/download/v1.1.2/kubernetes.tar.gz"
+  let archivePath = tempPath </> "kubernetes-1.1.2.tar.gz"
+  writeFile archivePath (response ^. responseBody)
+  return archivePath
+
+extractRelease :: FilePath -> FilePath -> IO ()
+extractRelease srcFile destDir = do
+  unpack destDir . read . decompress =<< readFile srcFile
 
 correctPrerequisite :: Int -> String -> IO ()
 correctPrerequisite exit msg = case exit of
